4b16a884c7ed77b4d2089b274055ee47d9ba202e
Ye Yixin <ye_yi_xin@live.com>
2020-04-16 15:43:05 -0700

    edited plot.py

--- gmplot/plot.py
+++ gmplot/plot.py
@@ -21,57 +21,57 @@ def bearingLine(lat,lon,bearing,d=.05):
     elif bearing > 0 and bearing < 90:
         rads = math.radians(bearing)
         tan = math.tan(rads)
-        
+
         adj = math.sqrt((d**2)/(1+tan**2))
         b_lat = lat + adj
-        
+
         opp = math.sqrt((tan**2*d**2)/(1+tan**2))
         b_lon = lon + opp
-    
+
     elif bearing > 90 and bearing < 180:
         rads = math.radians(180-bearing)
         tan = math.tan(rads)
-        
+
         adj = math.sqrt((d**2)/(1+tan**2))
         b_lat = lat - adj
-        
+
         opp = math.sqrt((tan**2*d**2)/(1+tan**2))
         b_lon = lon + opp
 
     elif bearing > 180 and bearing < 270:
-        rads = math.radians(bearing-270)
+        rads = math.radians(bearing-180)
         tan = math.tan(rads)
-        
+
         adj = math.sqrt((d**2)/(1+tan**2))
         b_lat = lat - adj
-        
+
         opp = math.sqrt((tan**2*d**2)/(1+tan**2))
         b_lon = lon - opp
 
     elif bearing > 270 and bearing < 360:
         rads = math.radians(360-bearing)
         tan = math.tan(rads)
-        
+
         adj = math.sqrt((d**2)/(1+tan**2))
         b_lat = lat + adj
-        
+
         opp = math.sqrt((tan**2*d**2)/(1+tan**2))
         b_lon = lon - opp
 
-    return (b_lat,b_lon)
+    lat_list = [lat,b_lat]
+    lon_list = [lon,b_lon]
 
-lat = 39.0833
-lon = -76.5755
+    gmap1.marker(lat,lon,'cornflowerblue')
+    gmap1.plot(lat_list,lon_list,'cornflowerblue',edge_width=3)
 
-p = bearingLine(lat,lon,278.4)
+    return
 
-lat_list = [lat,p[0]]
-lon_list = [lon,p[1]]
-
-gmap1 = gmplot.GoogleMapPlotter(39.0833,-76.5755,17)
+#Tells Google Map where to
+gmap1 = gmplot.GoogleMapPlotter(38.9837275,-76.4855159,18)
 gmap1.apikey = 'AIzaSyBKGW68OEyogCQxQTLqoaPKXxmBpV4kLkA'
 
-gmap1.marker(lat,lon)
-gmap1.plot(lat_list,lon_list)
+bearingLine(38.983400,-76.484368,280)
+bearingLine(38.984117,-76.485747,185)
+bearingLine(38.983025,-76.486616,54)
 
 gmap1.draw('map.html')
